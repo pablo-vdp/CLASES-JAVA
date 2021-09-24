9e7c4aa583200e0ee1fef52ae0aba4ea973e5280
pablo-vdp <pabloillescas8@gmail.com>
2021-09-24 15:47:45 +0200

    Ej array meses

--- JavaApp/src/javaapp/JavaApp.java
+++ JavaApp/src/javaapp/JavaApp.java
@@ -15,7 +15,26 @@ public class JavaApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i = 0;
+        int ArrayMeses[][] = new int [12][];
+        
+        for(int i = 0;ArrayMeses.length < i ;i++){
+            if (i==1){
+                ArrayMeses[1]=new int [28]; //reservar espacios en el 1er Array (2da POS)
+            }else if (i%2==0){
+                ArrayMeses[i]=new int [31]; // meses pares = 31 dias
+            }else{
+                ArrayMeses[i]=new int [30]; //meses impares = 30 
+            }
+        }
+        
+       //relelnar Arrays
+       
+       for (int i = 0;ArrayMeses.length < i ;i++){          //recorrer cada array
+           for (int j = 0;ArrayMeses[i].length < j ;j++){   //dentro de cada array recorrer cada pos
+               ArrayMeses[i][j]=j+1;                        //añadir num a cada espacio/pos reservado
+               System.out.println(ArrayMeses[i][j]);
+               System.out.println("HOLITA");
+           }  System.out.println("HOLA"+i);  
+        }System.out.println("HOLI");
     }
-    
 }
